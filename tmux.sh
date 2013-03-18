f986e55e9804340c9ba65fd194bd985d2b891484
Simon Constans <simonc@theodo.fr>
2013-03-18 19:25:20 +0100

    create tmux project

--- tmux.sh
+++ tmux.sh
@@ -1,19 +1,43 @@
 #!/bin/bash
-# This should work whether you are already in a TMUX session or not...
-# Irssi directory is assumed to be in the user's home dir
 
 create_tmux_project() {
     tmux new-window -t $1:$2 -n $3
+    tmux send-keys -t $1:$2 "cd $4; clear" C-m
     tmux split-window -t $1:$2 -h -p 25
+    tmux send-keys -t $1:$2 "cd $4; clear" C-m
     tmux split-window -t $1:$2 -v -p 66
+    tmux send-keys -t $1:$2 "cd $4; clear" C-m
     tmux split-window -t $1:$2 -v -p 50
-    tmux send-keys -t $1:$2 C+t
+    tmux send-keys -t $1:$2 "cd $4; clear" C-m
     tmux attach-session -t $1
 }
 
+create_tmux_project_nbi() {
+    create_tmux_project home 2 nbi ~/dev/projects/nosBelIdees
+}
+
+create_tmux_project_lodp() {
+    create_tmux_project home 2 lodp ~/dev/projects/lesoiseauxdepassage
+}
+
+create_tmux_project_fatumfatras() {
+    create_tmux_project home 2 fatumfatras ~/dev/projects/fatumfatras
+}
 
 if [ -z "$TMUX" ]
 then
     tmux new-session -d -s home
-    create_tmux_project home 2 lodp
+
+    # pro
+    if [ $USER = 'simonc' ]
+    then
+        create_tmux_project_nbi
+    fi
+
+    # perso
+    if [ $USER = 'kosssi' ]
+    then
+        create_tmux_project_lodp
+        create_tmux_project_fatumfatras
+    fi
 fi
